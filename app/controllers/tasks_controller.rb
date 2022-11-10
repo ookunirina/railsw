59b17488ffedff0da7efb3caffe00b6b51370481
rina <purotoinu@icloud.com>
2022-11-10 22:54:29 +0900

    Before try_byebug

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,7 +28,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    task = Task.find(params[:id]
+    task = Task.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を消除しました。"
   end
