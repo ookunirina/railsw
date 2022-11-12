b34101e3d4b97218078177fb9e8fc80909274870
rina <purotoinu@icloud.com>
2022-11-12 14:33:25 +0900

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_06_053945) do
+ActiveRecord::Schema.define(version: 2022_11_12_040431) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
