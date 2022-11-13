a6f64c1837977681a6a1ec26c9df880384e4788c
rina <purotoinu@icloud.com>
2022-11-13 13:59:55 +0900

    first commit3

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
+# validates :name, presence: trueとvalidates :name, length: { maximum: 30 }を合わせてvalidates :name, presence: true, length:  { maximum: 30 }にできる
